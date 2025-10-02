abcc50454ad3464ba5639c14295b544932b10f5f
Bakberdi <bakberdy.ye@gmail.com>
2025-10-02 19:58:39 +0500

    Initial Commit

---
+++ find pair/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  find pair
+//
+//  Created by Bakberdi Esentai on 02.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
